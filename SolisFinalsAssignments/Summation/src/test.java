b4f6f1ccd86b99811c2a6c49b7f50f8314efd2f0
Alexander Jon <74027875+Alexander040105@users.noreply.github.com>
2024-06-08 19:02:44 +0800

    summation of elements of array

---
+++ SolisFinalsAssignments/Summation/src/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args){
+        System.out.println("-----------------------\n");
+
+        for(int ctr=0; ctr<5; ctr++){
+            System.out.println("num["+ ctr +"] = \t" + num[ctr]);
+            int sum = sum + num[ctr];
+        }
+    }
+}
